bd54ad2943c6801a643f32ed4ee1cf336c60cd0f
Lothriiik <uanderson_pb@hotmail.com.br>
2026-01-20 23:33:58 -0300

    fix: import login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import HomeLayout from "@/layouts/HomeLayout/HomeLayout"
-import Login from "@/pages/Login/index.jsx"
-import Cadastro from "@/pages/Cadastro/index.jsx"
+import Login from "@/pages/Login"
+import Cadastro from "@/pages/Cadastro"
 import { Toaster } from "sonner"
 
 export default function App() {
@@ -10,7 +10,7 @@ export default function App() {
       <Toaster position="bottom-right" richColors theme="dark" />
 
       <Routes>
-        <Route path="/" element={<Navigate to="/home/pomodoro" />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />}/>
         <Route path="/cadastro" element={<Cadastro/>}/>
         <Route path="/home/*" element={<HomeLayout />} />
